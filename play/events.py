0f216f85e5c27c9e1db5180e04f7e4dee50e367e
ducaale <sharaf.13@hotmail.com>
2020-02-23 21:47:41 +0000

    fix broadcast_and_wait bug

--- play/events.py
+++ play/events.py
@@ -1,45 +1,43 @@
 import asyncio
 from .utils import make_async
 
-custom_events = []
+custom_events = set()
 custom_event_callbacks = []
 
-def still_running_callbacks(event):
+def running_callbacks(event):
     for callback in custom_event_callbacks:
         if callback.event == event and callback.is_running:
             return True
     return False
 
-
 def broadcast(event='default'):
-    custom_events.append(event)
-
+    custom_events.add(event)
 
 async def broadcast_and_wait(event='default'):
-    custom_events.append(event)
-    while still_running_callbacks(event):
-        await asyncio.sleep(0)
+    custom_events.add(event)
+    await asyncio.sleep(0)
+    while running_callbacks(event): await asyncio.sleep(0)
 
+def wrap_promise(async_func):
+    async def wrapper(*args, **kwargs):
+        await async_func(*args, **kwargs)
+        wrapper.is_running = False
+    wrapper.is_running = False
+    return wrapper
 
 # @decorator
 def when_event_recieved(event):
     def decorator(func):
-        async_callback = make_async(func)
-        # TODO: move wrapper to utils
-        async def wrapper(*args, **kwargs):
-            wrapper.is_running = True
-            await async_callback(*args, **kwargs)
-            wrapper.is_running = False
-        wrapper.is_running = False
+        wrapper = wrap_promise(make_async(func))
         wrapper.event = event
         custom_event_callbacks.append(wrapper)
         return wrapper
     return decorator
 
-# TODO: fix broadcast_and_wait not working correctly
 def process_custom_event_callbacks(loop):
     for callback in custom_event_callbacks:
         if not callback.is_running and callback.event in custom_events:
+            callback.is_running = True
             loop.create_task(callback())
 
     custom_events.clear()
