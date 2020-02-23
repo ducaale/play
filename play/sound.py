b4a4cf649a708d29f2faef7d66b079000e7a6e78
ducaale <sharaf.13@hotmail.com>
2020-02-23 22:10:19 +0000

    add default sound

--- play/sound.py
+++ play/sound.py
@@ -1,12 +1,15 @@
+import os
 import pygame.mixer
 
 class Sound():
-    def __init__(self, sound):
-        self.sound = pygame.mixer.Sound(sound)
+    def __init__(self, sound=None):
+        path = sound or os.path.join(
+            os.path.split(__file__)[0], 'a_tone.wav')
+        self.sound = pygame.mixer.Sound(path)
 
     def play(self):
         self.sound.play()
 
 
-def new_sound(sound):
+def new_sound(sound=None):
     return Sound(sound)
